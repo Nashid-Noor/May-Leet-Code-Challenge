a8ffcfa4bb2d6a638a1ed6de5a13d3201a7f2e3d
Nashid-Noor <62026866+Nashid-Noor@users.noreply.github.com>
2020-05-05 14:10:06 +0530

    Delete Day_5_First_Unique_Characters_in_a_String.py

--- Day_5_First_Unique_Characters_in_a_String.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        c=Counter(s)
-        for i in c:
-            if c[i]==1:            
-                return s.find(i)
-        return -1
-        
